4e8dbead72aa968dd26be755d124dc7bd83f0ce9
fyz1994 <18106987196@163.com>
2019-11-21 03:01:46 +0800

    change: 修正评论组件

--- builds/findMarkdown.js
+++ builds/findMarkdown.js
@@ -1,22 +1,25 @@
-const fs = require("fs");
+const fs = require('fs')
 
 function findMarkdown(dir, callback) {
   fs.readdir(dir, function (err, files) {
-    if (err) throw err;
-    files.forEach(fileName => {
-      let innerDir = `${dir}/${fileName}`;
-      if (fileName.indexOf(".") !== 0) {
+    if (err) throw err
+
+    files.forEach((fileName) => {
+      let innerDir = `${dir}/${fileName}`
+
+      if (fileName.indexOf('.') !== 0) {
         fs.stat(innerDir, function (err, stat) {
+
           if (stat.isDirectory()) {
-            findMarkdown(innerDir, callback);
+            findMarkdown(innerDir, callback)
           } else {
-            // 跳过readme 文件，当然你也可以自行修改
-            if (/\.md$/.test(fileName) && !/README/.test(fileName))
-              callback(innerDir);
+            callback(innerDir)
           }
-        });
+        })
       }
-    });
-  });
+
+    })
+  })
 }
-module.exports = findMarkdown;
+
+module.exports = findMarkdown
